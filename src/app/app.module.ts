5de705f4f935709e8d23f56be98444cd7f6ab338
Amil Pattni <amil.pattni@gmail.com>
2022-08-29 22:43:30 -0400

    We got spacing correct and add search
    Co-authored-by: Aashiqin <Aashiqin@users.noreply.github.com>
    Co-authored-by: Ryan Rambali <ryanrambali@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbInputModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
@@ -21,6 +21,7 @@ import { NavComponent } from './nav/nav.component';
 
   ],
   imports: [
+    NbInputModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
